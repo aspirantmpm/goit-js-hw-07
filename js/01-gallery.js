8aad116e3092ba449cd5cd71aa4a6d6b571ca3cb
Vadym <aspirantmpm@gmail.com>
2022-12-14 19:10:22 +0200

    First task

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join('');
+}
+
+gallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const originalSize = basicLightbox.create(`
+    <img class="gallery__image" src="${evt.target.dataset.source}" alt="${evt.target.description}" width="800" height="600"/>
+`);
+  
+  document.addEventListener('keydown', onClickClose);
+  originalSize.show();
+  console.log(evt.target);
+}
+console.log(basicLightbox);
+function onClickClose(evt) {
+  if (evt.code === 'Escape') {
+    originalSize.close();
+    documents.removeEventListener('keydown', onClickClose);
+  }
+}
